2b544ea6c4c4cc3db939edf423882b72c0b4d10c
digitallysavvy <hermes.frangoudis@gmail.com>
2019-08-08 03:15:02 -0400

    fixed errors related to objc wrapped class, and missing imports (foundation, uikit), and renamed enums

--- Pod/Classes/RecordButton.swift
+++ Pod/Classes/RecordButton.swift
@@ -3,14 +3,18 @@
 //  Instant
 //
 //  Created by Samuel Beek on 21/06/15.
+//  Updated by digitallysavvy on (22/03/18), (08/08/19)
 //  Copyright (c) 2015 Samuel Beek. All rights reserved.
 //
+
+import Foundation
+import UIKit
 
 @objc public enum RecordButtonState : Int {
     case recording, idle, hidden;
 }
 
-@objc open class RecordButton : UIButton {
+open class RecordButton : UIButton {
     
     open var buttonColor: UIColor! = .blue{
         didSet {
@@ -50,7 +54,7 @@
     fileprivate var progressLayer: CAShapeLayer!
     fileprivate var gradientMaskLayer: CAGradientLayer!
     fileprivate var currentProgress: CGFloat! = 0
-
+    
     
     override public init(frame: CGRect) {
         
@@ -118,30 +122,30 @@
     fileprivate func setRecording(_ recording: Bool) {
         
         let duration: TimeInterval = 0.15
-        circleLayer.contentsGravity = "center"
+        circleLayer.contentsGravity = CALayerContentsGravity(rawValue: "center")
         
         let scale = CABasicAnimation(keyPath: "transform.scale")
         scale.fromValue = recording ? 1.0 : 0.88
         scale.toValue = recording ? 0.88 : 1
         scale.duration = duration
-        scale.fillMode = kCAFillModeForwards
+        scale.fillMode = CAMediaTimingFillMode.forwards
         scale.isRemovedOnCompletion = false
         
         let color = CABasicAnimation(keyPath: "backgroundColor")
         color.duration = duration
-        color.fillMode = kCAFillModeForwards
+        color.fillMode = CAMediaTimingFillMode.forwards
         color.isRemovedOnCompletion = false
         color.toValue = recording ? progressColor.cgColor : buttonColor.cgColor
         
         let circleAnimations = CAAnimationGroup()
         circleAnimations.isRemovedOnCompletion = false
-        circleAnimations.fillMode = kCAFillModeForwards
+        circleAnimations.fillMode = CAMediaTimingFillMode.forwards
         circleAnimations.duration = duration
         circleAnimations.animations = [scale, color]
         
         let borderColor: CABasicAnimation = CABasicAnimation(keyPath: "borderColor")
         borderColor.duration = duration
-        borderColor.fillMode = kCAFillModeForwards
+        borderColor.fillMode = CAMediaTimingFillMode.forwards
         borderColor.isRemovedOnCompletion = false
         borderColor.toValue = recording ? UIColor(red: 0.83, green: 0.86, blue: 0.89, alpha: 1).cgColor : buttonColor
         
@@ -149,12 +153,12 @@
         borderScale.fromValue = recording ? 1.0 : 0.88
         borderScale.toValue = recording ? 0.88 : 1.0
         borderScale.duration = duration
-        borderScale.fillMode = kCAFillModeForwards
+        borderScale.fillMode = CAMediaTimingFillMode.forwards
         borderScale.isRemovedOnCompletion = false
         
         let borderAnimations = CAAnimationGroup()
         borderAnimations.isRemovedOnCompletion = false
-        borderAnimations.fillMode = kCAFillModeForwards
+        borderAnimations.fillMode = CAMediaTimingFillMode.forwards
         borderAnimations.duration = duration
         borderAnimations.animations = [borderColor, borderScale]
         
@@ -162,7 +166,7 @@
         fade.fromValue = recording ? 0.0 : 1.0
         fade.toValue = recording ? 1.0 : 0.0
         fade.duration = duration
-        fade.fillMode = kCAFillModeForwards
+        fade.fillMode = CAMediaTimingFillMode.forwards
         fade.isRemovedOnCompletion = false
         
         circleLayer.add(circleAnimations, forKey: "circleAnimations")
@@ -200,9 +204,9 @@
             
             UIView.animate(withDuration: 0.3, animations: {
                 self.buttonState = .hidden
-                }, completion: { completion in
-                    self.setProgress(0)
-                    self.currentProgress = 0
+            }, completion: { completion in
+                self.setProgress(0)
+                self.currentProgress = 0
             })
         } else {
             self.buttonState = .idle
@@ -211,14 +215,12 @@
     
     
     /**
-    Set the relative length of the circle border to the specified progress
-    
-    - parameter newProgress: the relative lenght, a percentage as float.
-    */
+     Set the relative length of the circle border to the specified progress
+     
+     - parameter newProgress: the relative lenght, a percentage as float.
+     */
     open func setProgress(_ newProgress: CGFloat) {
         progressLayer.strokeEnd = newProgress
     }
     
-    
 }
-
